dc2aad07ee04750237bbf075920f0d7da8f2665f
Roberto Garza <robertg4567@hotmail.com>
2018-07-17 13:39:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function breakOut(array, changeValue, stopValue){
   for(var i =0;i<stopValue;i++){
     if(array[i] == stopValue)
       break
-      
+
     array[i] = changeValue
   }
   return array
